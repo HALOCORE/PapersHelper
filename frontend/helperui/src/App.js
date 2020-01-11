f904e1cb1a78af60cc430aa48944a941f93fa974
Bo Wang <bwcs@uchicago.edu>
2020-01-11 12:15:29 +0800

    first component

--- frontend/helperui/src/App.js
+++ frontend/helperui/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
-import FolderList from './components/folderlist/FolderList';
+import FolderList from './components/folderlist/FolderList.js';
 
 function App() {
   return (
-    <div>
+    <div className="app-root">
       <FolderList></FolderList>
       <Button variant="contained" color="primary">
         你好，世界
